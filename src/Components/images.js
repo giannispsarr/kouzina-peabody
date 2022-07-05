402624785b1024ca97c3e12eb039fd6c9ab04ec7
giannispsarr <57642467+giannispsarr@users.noreply.github.com>
2022-07-05 21:34:01 +0300

    Add files via upload

--- src/Components/images.js
+++ src/Components/images.js
@@ -1,16 +1,32 @@
 
-import gallery01 from '../assets/gallery01.jpg';
-import gallery02 from '../assets/gallery02.jpg';
-import gallery03 from '../assets/gallery03.jpg';
-import gallery04 from '../assets/gallery04.jpg';
-import gallery05 from '../assets/gallery05.jpg';
-import gallery06 from '../assets/gallery06.jpg';
+import gallery1 from '../assets/gallery1.jpg';
+import gallery2 from '../assets/gallery2.jpg';
+import gallery3 from '../assets/gallery3.jpg';
+import gallery4 from '../assets/gallery4.jpg';
+import gallery5 from '../assets/gallery5.jpg';
+import gallery6 from '../assets/gallery6.jpg';
+import gallery7 from '../assets/gallery7.jpg';
+import gallery8 from '../assets/gallery8.jpg';
+import gallery9 from '../assets/gallery9.jpg';
+import gallery10 from '../assets/gallery10.jpg';
+import gallery11 from '../assets/gallery11.jpg';
+import gallery12 from '../assets/gallery12.jpg';
+import gallery13 from '../assets/gallery13.jpg';
+import gallery14 from '../assets/gallery14.jpg';
 
 export default {
-    gallery01,
-    gallery02,
-    gallery03,
-    gallery04,
-    gallery05,
-    gallery06
+    gallery1,
+    gallery2,
+    gallery3,
+    gallery4,
+    gallery5,
+    gallery6,
+    gallery7,
+    gallery8,
+    gallery9,
+    gallery10,
+    gallery11,
+    gallery12,
+    gallery13,
+    gallery14
   };
