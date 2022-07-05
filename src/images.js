25f1e5feb75ecbadbb5b9f19997fb70dac25988e
giannispsarr <57642467+giannispsarr@users.noreply.github.com>
2022-07-05 21:42:32 +0300

    New Images

---
+++ src/images.js
@@ -0,0 +1,32 @@
+
+import gallery1 from '../assets/gallery1.jpg';
+import gallery2 from '../assets/gallery2.jpg';
+import gallery3 from '../assets/gallery3.jpg';
+import gallery4 from '../assets/gallery4.jpg';
+import gallery5 from '../assets/gallery5.jpg';
+import gallery6 from '../assets/gallery6.jpg';
+import gallery7 from '../assets/gallery7.jpg';
+import gallery8 from '../assets/gallery8.jpg';
+import gallery9 from '../assets/gallery9.jpg';
+import gallery10 from '../assets/gallery10.jpg';
+import gallery11 from '../assets/gallery11.jpg';
+import gallery12 from '../assets/gallery12.jpg';
+import gallery13 from '../assets/gallery13.jpg';
+import gallery14 from '../assets/gallery14.jpg';
+
+export default {
+    gallery1,
+    gallery2,
+    gallery3,
+    gallery4,
+    gallery5,
+    gallery6,
+    gallery7,
+    gallery8,
+    gallery9,
+    gallery10,
+    gallery11,
+    gallery12,
+    gallery13,
+    gallery14
+  };
